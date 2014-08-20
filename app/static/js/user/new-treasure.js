024d6bf97f2584cd48c49948ec283446af5af9db
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-20 17:45:37 -0500

    locations found

--- app/static/js/user/new-treasure.js
+++ app/static/js/user/new-treasure.js
@@ -5,11 +5,40 @@
 
   $(document).ready(function(){
     $('#addHint').click(addHint);
+    $('form').submit(addTreasure);
   });
 
   function addHint(){
     var $input = "<input type='text', name='hints' class='form-control'/>";
     $('#hints').append($input);
+  }
+
+  function send(){
+    alert('alert');
+  }
+
+  function addTreasure(e){
+    var lat = $('#lat').val();
+    if(!lat){
+      var name = $('#loc').val();
+      geocode(name);
+      e.preventDefault();
+    }
+  }
+
+  function geocode(address){
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address:address}, function(results, status){
+      var name = results[0].formatted_address,
+          lat = results[0].geometry.location.lat(),
+          lng = results[0].geometry.location.lng();
+
+     $('#loc').val(name);
+     $('#lat').val(lat);
+     $('#lng').val(lng);
+     $('form').submit();
+
+   });
   }
 
 })();
